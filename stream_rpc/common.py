d655a82434391485574ff25ba8108189ffc4ce59
Andrew Burdyug <buran83@gmail.com>
2019-11-19 19:46:56 +0300

    Add possibility to use custom cypher class

--- stream_rpc/common.py
+++ stream_rpc/common.py
@@ -13,6 +13,7 @@ from stream_rpc.crypto import Cypher
 STREAM_RPC_HOST = os.getenv("STREAM_RPC_HOST", "0.0.0.0")
 STREAM_RPC_PORT = int(os.getenv("STREAM_RPC_HOST", "8888"))
 STREAM_RPC_TERM = b"\n"
+STREAM_RPC_CYPHER = Cypher
 STREAM_RPC_LOG_LVL = getattr(logging, os.getenv("STREAM_RPC_LOG_LVL", "INFO"))
 STREAM_RPC_LOG_FMT = (
     "%(asctime)-15s %(name)-5s\n  \\_ %(levelname)-8s %(message)s"
@@ -59,18 +60,32 @@ def create_message_packet(peer_id, request_number, method, payload=None):
 
 
 class Protocol(ABC):
-    """An abstract base class for Server/Client protocol classes."""
+    """An abstract base class for Server/Client protocol classes.
+
+    You should not use this class directly, you should create a new ones
+    (client/server) classes inherited from this and define in them the
+    `process_message` method.
+    """
 
     def __init__(
-        self, peer_id, reader, writer, message_separator=STREAM_RPC_TERM,
+        self,
+        peer_id,
+        reader,
+        writer,
+        message_separator=STREAM_RPC_TERM,
+        cypher=STREAM_RPC_CYPHER,
     ):
         """Initialize class.
 
-        :param str stream_rpc_host: Stream-RPC server address for connections
-        :param int stream_rpc_port: Stream-RPC port for connections
+        :param str peer_id: id of peer
+        :param asyncio.StreamReader reader: interface for reading from stream
+        :param asyncio.StreamWriter writer: interface for writing to stream
+        :param message_separator:
+        :param cypher:
+
 
         """
-        self.cypher = Cypher()
+        self.cypher = cypher()
         self.reader = reader
         self.writer = writer
         self.peer_id = peer_id
@@ -81,7 +96,7 @@ class Protocol(ABC):
         )
 
     def create_message_packet(self, *args, **kwargs):
-        """Create message packet using helpful function above."""
+        """Create message packet using helper function defined above."""
         return create_message_packet(
             self.peer_id, self.request_number, *args, **kwargs
         )
@@ -100,7 +115,7 @@ class Protocol(ABC):
         """Decrypt message.
 
         :param msg: previously encrypted message by `self.msg_encrypt()`
-        :returns: original message
+        :returns: origin message
 
         """
         return self.cypher.decrypt(msg)
@@ -119,7 +134,7 @@ class Protocol(ABC):
         """Send a message.
 
         Sending message to another side will expect response, which will be
-        processed in self.process_payload(), so after every sending
+        processed in self.process_message(), so after every sending
         self.read_message() will be execute.
 
         :param message_packet: a message packet
@@ -145,7 +160,8 @@ class Protocol(ABC):
         """Read message.
 
         Reading message from another side involves execution of
-        self.process_payload(), define there all you needed.
+        `self.process_message()`, you chould not change this method,
+        you should define all you needed in `process_message()`.
 
         """
         try:
@@ -188,7 +204,13 @@ class Connector:
         stream_rpc_host=STREAM_RPC_HOST,
         stream_rpc_port=STREAM_RPC_PORT,
     ):
-        """Initialize class."""
+        """Initialize class.
+
+        :param protocol_class: a protocol class
+        :param str stream_rpc_host: Stream-RPC server address for connections
+        :param int stream_rpc_port: Stream-RPC port for connections
+
+        """
         self.protocol_class = protocol_class
         self.stream_rpc_host = stream_rpc_host
         self.stream_rpc_port = stream_rpc_port
@@ -224,7 +246,12 @@ class Connector:
             await server.serve_forever()
 
     async def run_client(self, hello_message):
-        """Run client."""
+        """Run client.
+
+        :param `stream_rpc.common.MessagePacket` hello_message: a hello message,
+        which client will send at the time of first connection.
+
+        """
         reader, writer = await asyncio.open_connection(
             self.stream_rpc_host, self.stream_rpc_port
         )
